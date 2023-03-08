f15a69fc1bc387054449731fc5657983e9417018
notemmu <emilieinzinna@gmail.com>
2023-03-08 16:23:29 -0500

    new motor controllers

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,29 +1,36 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class DriveSubsystem extends SubsystemBase {
-    private VictorSP leftDrive = new VictorSP(0);
-    private VictorSP rightDrive = new VictorSP(1);
+    private WPI_TalonSRX frontLeftDrive = new WPI_TalonSRX(11);
+    private WPI_TalonSRX frontRightDrive = new WPI_TalonSRX(12);
+    private WPI_VictorSPX rearLeftDrive = new WPI_VictorSPX(13);
+    private WPI_VictorSPX rearRightDrive = new WPI_VictorSPX(14);
     private CommandXboxController driveController;
 
-    private DifferentialDrive drive = new DifferentialDrive(leftDrive, rightDrive);
+    private DifferentialDrive drive = new DifferentialDrive(frontLeftDrive, frontRightDrive);
     public DifferentialDrive getDrive() {
         return drive;
     }
     
     public DriveSubsystem(CommandXboxController m_driverController){
         this.driveController = m_driverController;
-        rightDrive.setInverted(true);
+
+        rearLeftDrive.follow(frontLeftDrive);
+        rearRightDrive.follow(frontRightDrive);
+
+        frontLeftDrive.setNeutralMode(NeutralMode.Brake);
+        frontRightDrive.setNeutralMode(NeutralMode.Brake);
+        rearLeftDrive.setNeutralMode(NeutralMode.Brake);
+        rearRightDrive.setNeutralMode(NeutralMode.Brake);
     }
     
     public void arcadeDrive(double xSpeed, double zRotation) {
